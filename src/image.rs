169145c35b0123a7d9d4e04531ae182d6bb69b2e
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2023-10-23 17:47:39 +0100

    webp conversion

--- src/image.rs
+++ src/image.rs
@@ -1,11 +1,16 @@
 use std::{
-    fs,
+    fs::{self, File},
+    io::Write,
     path::{Path, PathBuf},
+    ptr,
     sync::Arc,
 };
 
 use bytes::Bytes;
 use cached::proc_macro::cached;
+use exif::{In, Reader, Tag};
+use image::DynamicImage;
+use libwebp_sys::WebPEncodeRGB;
 use octocrab::{models::repos::Object, params::repos::Reference};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
@@ -83,28 +88,34 @@ pub async fn image_from_github_cached(
     Image::from_github(uuid, ext, &config).await
 }
 
-pub fn create_new_image(config: &AppConfig, image: PathBuf, description: String) {
-    let image_index_path = Path::new(&config.image_index_file_path);
+pub fn create_new_image(config: &AppConfig, path: PathBuf, description: String) {
+    // create an ImageEntry -> will always be .webp ext
+    let index_entry = ImageEntry::new(description, Some("webp".into()));
+    if let Some(ext) = path.extension() {
+        let fname = format!(
+            "{}/{}.webp",
+            config.image_index_base_path,
+            index_entry.id.to_string()
+        );
+        if ext != "webp" {
+            // convert it and store in right place
+            let img = image::open(&path).expect("Should be able to open image");
+            let img = correct_orientation(img, &path).expect("Should be able to convert");
+            let img_webp_raw = convert_to_webp(img, 0.75);
 
-    let ext = image.extension();
-
-    let index_entry = if let Some(ext) = ext {
-        ImageEntry::new(description, Some(ext.to_str().unwrap().to_string()))
+            let mut file = File::create(fname).expect("should be able to create file");
+            file.write_all(&img_webp_raw)
+                .expect("should be able to write image data");
+        } else {
+            // just store it in right place
+            let _ = fs::copy(path, fname);
+        }
     } else {
-        ImageEntry::new(description, None)
-    };
-
-    let mut fname = format!(
-        "{}/{}",
-        config.image_index_base_path,
-        index_entry.id.to_string()
-    );
-    if let Some(ext) = ext {
-        fname.push_str(".");
-        fname.push_str(ext.to_str().unwrap_or(""))
+        // there is no extension so lets actually panic?
+        panic!("There needs to be an extension on the image!!")
     }
-
-    let _ = fs::copy(image, fname);
+    // Add ImageEntry to Index
+    let image_index_path = Path::new(&config.image_index_file_path);
     if let Ok(mut index) = Index::from_file(&image_index_path) {
         index.0.push(index_entry);
         index.to_file(&image_index_path).unwrap()
@@ -121,4 +132,52 @@ pub async fn image_index_from_github_cached(
     path: String,
 ) -> Result<Index<ImageEntry>, String> {
     Index::<ImageEntry>::from_github(&config, path).await
+}
+
+pub fn convert_to_webp(img: DynamicImage, quality: f32) -> Vec<u8> {
+    let img = img.to_rgb8();
+    let h = img.height();
+    let w = img.width();
+    let data = img.into_raw();
+
+    let out = unsafe {
+        let mut output_data = ptr::null_mut();
+        let stride = w as i32 * 3;
+        let len = WebPEncodeRGB(
+            data.as_ptr(),
+            w as i32,
+            h as i32,
+            stride,
+            quality,
+            &mut output_data,
+        );
+        std::slice::from_raw_parts(output_data, len as usize).into()
+    };
+    out
+}
+
+pub fn correct_orientation(img: DynamicImage, img_path: &Path) -> Result<DynamicImage, String> {
+    let file = std::fs::File::open(img_path).unwrap();
+    let mut bufreader = std::io::BufReader::new(&file);
+    let exifreader = exif::Reader::new();
+    let exif_data = exifreader.read_from_container(&mut bufreader).unwrap();
+
+    let orientation_field = exif_data.get_field(Tag::Orientation, In::PRIMARY);
+    if let Some(field) = orientation_field {
+        if let Some(orientation) = field.value.get_uint(0) {
+            return Ok(match orientation {
+                1 => img,
+                2 => img.fliph(),
+                3 => img.rotate180(),
+                4 => img.rotate180().fliph(),
+                5 => img.rotate90().fliph(),
+                6 => img.rotate90(),
+                7 => img.rotate270().fliph(),
+                8 => img.rotate270(),
+                _ => img, // Keep the original image if orientation value is unknown
+            });
+        }
+    }
+
+    Ok(img)
 }
